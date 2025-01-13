a6b3032f1c683093a3a5f7fb58e0e09b0eb81030
felix steen <felixsteen@gmail.com>
2025-01-13 08:38:13 +0100

    bootstrap

--- BlazorRSS/Program.cs
+++ BlazorRSS/Program.cs
@@ -1,12 +1,15 @@
 using BlazorRSS.Components;
 using BlazorRSS.Services;
+using @BlazorBootstrap;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddHttpClient<RssFeedService>();
+builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
