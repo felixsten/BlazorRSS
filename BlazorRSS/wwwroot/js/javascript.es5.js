7f95fdd5ef2881dbaed4f1188003e972964ab298
Felix <felixsteen@gmail.com>
2025-01-27 09:00:55 +0100

    js

---
+++ BlazorRSS/wwwroot/js/javascript.es5.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+window.startAutoScroll = function (containerId) {
+
+    setTimeout(function () {
+        var container = document.getElementById(containerId);
+
+        if (container) {
+            (function () {
+                var scrollContent = function scrollContent() {
+                    if (isScrollingDown) {
+                        if (scrollAmount >= container.scrollHeight - container.clientHeight) {
+                            isScrollingDown = false;
+                        } else {
+                            scrollAmount += scrollStep;
+                        }
+                    } else {
+                        if (scrollAmount <= 0) {
+                            isScrollingDown = true;
+                        } else {
+                            scrollAmount -= scrollStep;
+                        }
+                    }
+                    container.scrollTop = scrollAmount;
+                    window.requestAnimationFrame(scrollContent);
+                };
+
+                console.log("Container found! Starting auto-scroll.");
+
+                var scrollAmount = 0;
+                var scrollStep = 2;
+                var isScrollingDown = true;
+
+                window.requestAnimationFrame(scrollContent);
+            })();
+        } else {
+            console.log('Container not found');
+        }
+    }, 500);
+};
+
---
+++ BlazorRSS/wwwroot/js/javascript.js
@@ -0,0 +1,38 @@
+﻿
+window.startAutoScroll = (containerId) => {
+    
+    setTimeout(() => {
+        const container = document.getElementById(containerId);
+
+        if (container) {
+            console.log("Container found! Starting auto-scroll.");
+
+            let scrollAmount = 0;
+            let scrollStep = 2; 
+            let isScrollingDown = true;
+
+            function scrollContent() {
+                if (isScrollingDown) {
+                    if (scrollAmount >= container.scrollHeight - container.clientHeight) {
+                        isScrollingDown = false; 
+                    } else {
+                        scrollAmount += scrollStep;
+                    }
+                } else {
+                    if (scrollAmount <= 0) {
+                        isScrollingDown = true; 
+                    } else {
+                        scrollAmount -= scrollStep;
+                    }
+                }
+                container.scrollTop = scrollAmount;
+                window.requestAnimationFrame(scrollContent);
+            }
+
+            
+            window.requestAnimationFrame(scrollContent);
+        } else {
+            console.log('Container not found');
+        }
+    }, 500); 
+};
